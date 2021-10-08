1b6d6eed0b08f26f28fe7035a2f079c4e495bc54
halidguid <halidhosi@gmail.com>
2021-10-08 14:08:45 +0200

    friyay

---
+++ src/components/Game.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+import Dice from "./Dice";
+
+import "./Game.scss";
+import ScoreTable from "./ScoreTable";
+
+const NUM_DICE = 5;
+const NUM_ROLLS = 2;
+
+function Game() {
+  const [gameState, setGameState] = useState({
+    dice: Array.from({ length: NUM_DICE }),
+    locked: Array(NUM_DICE).fill(false),
+    rollsLeft: NUM_ROLLS,
+    rolling: false,
+    scores: {
+      ones: undefined,
+      twos: undefined,
+      threes: undefined,
+      fours: undefined,
+      fives: undefined,
+      sixes: undefined,
+      threeOfKind: undefined,
+      fourOfKind: undefined,
+      fullHouse: undefined,
+      smallStraight: undefined,
+      largeStraight: undefined,
+      yahtzee: undefined,
+      chance: undefined,
+    },
+  });
+  const [counter, setCounter] = useState(0);
+
+  function roll(event) {
+    // roll dice whose indexes are in reroll
+    setGameState((gs) => ({
+      ...gameState,
+      dice: gs.dice.map((d, i) =>
+        gs.locked[i] ? d : Math.ceil(Math.random() * 6)
+      ),
+      locked: gs.rollsLeft > 1 ? gs.locked : Array(NUM_DICE).fill(true),
+      rollsLeft: gs.rollsLeft > 0 ? gs.rollsLeft - 1 : 0,
+      rolling: false,
+    }));
+  }
+
+  function animateRoll() {
+    setGameState(() => ({
+      ...gameState,
+      rolling: true,
+    }));
+    setTimeout(roll, 1000);
+  }
+
+  function toggleLocked(idx) {
+    // toggle whether idx is in locked or not
+
+    if (gameState.rollsLeft > 0 && !gameState.rolling) {
+      setGameState((gs) => ({
+        ...gameState,
+        locked: [
+          ...gs.locked.slice(0, idx),
+          !gs.locked[idx],
+          ...gs.locked.slice(idx + 1),
+        ],
+      }));
+    }
+  }
+
+  function doScore(ruleName, ruleFn) {
+    // evaluate this ruleFn with the dice and score this rulename
+    setGameState((gs) => ({
+      ...gameState,
+      scores: { ...gs.scores, [ruleName]: ruleFn(gameState.dice) },
+      rollsLeft: NUM_ROLLS,
+      locked: Array(NUM_DICE).fill(false),
+    }));
+    setCounter(counter + 1);
+    //animateRoll();
+  }
+
+  useEffect(() => {
+    if (counter === 13) {
+      setGameState((gs) => ({
+        ...gameState,
+        rollsLeft: 0,
+      }));
+    }
+
+    console.log(counter);
+  }, [counter]);
+
+  function displayRollInfo() {
+    const messages = [
+      "0 Rolls Left",
+      "1 Roll Left",
+      "2 Rolls Left",
+      "Starting Round",
+    ];
+    return messages[gameState.rollsLeft];
+  }
+
+  const resetHandler = () => {
+    setGameState({
+      dice: Array.from({ length: NUM_DICE }),
+      locked: Array(NUM_DICE).fill(false),
+      rollsLeft: NUM_ROLLS,
+      rolling: false,
+      scores: {
+        ones: undefined,
+        twos: undefined,
+        threes: undefined,
+        fours: undefined,
+        fives: undefined,
+        sixes: undefined,
+        threeOfKind: undefined,
+        fourOfKind: undefined,
+        fullHouse: undefined,
+        smallStraight: undefined,
+        largeStraight: undefined,
+        yahtzee: undefined,
+        chance: undefined,
+      },
+    });
+    setCounter(0);
+  };
+
+  return (
+    <div className="Game">
+      <header className="header">
+        <h1 className="title">
+          Yatzy <i className="fas fa-dice-six"></i>
+        </h1>
+        <section className="dice-section">
+          <Dice
+            dice={gameState.dice}
+            locked={gameState.locked}
+            handleClick={toggleLocked}
+            disabled={gameState.rollsLeft === 0}
+            rolling={gameState.rolling}
+          />
+          <div className="button-wrapper">
+            <button
+              disabled={
+                gameState.locked.every((x) => x) ||
+                gameState.rollsLeft === 0 ||
+                gameState.rolling
+              }
+              onClick={animateRoll}
+              className="reroll"
+            >
+              {displayRollInfo()}
+            </button>
+          </div>
+        </section>
+      </header>
+      <ScoreTable doScore={doScore} scores={gameState.scores} />
+      {counter === 13 ? (
+        <button className="reroll" onClick={resetHandler}>
+          Start New Game
+        </button>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+}
+
+export default Game;
